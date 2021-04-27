6eae3fcda6017a244daa40f93185928e8e00c45b
Tris666w <55093987+Tris666w@users.noreply.github.com>
2021-04-27 21:01:37 +0200

    Week 9 complete

--- OverlordEngine/OverlordEngine/Particle.cpp
+++ OverlordEngine/OverlordEngine/Particle.cpp
@@ -16,12 +16,61 @@ Particle::Particle(const ParticleEmitterSettings& emitterSettings):
 
 void Particle::Update(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
+	if (!m_IsActive)
+	{
+		return;
+	}
+
+	m_CurrentEnergy -= gameContext.pGameTime->GetElapsed();
+	if (m_CurrentEnergy <= 0.f)
+	{
+		m_IsActive =false;
+		return;
+	}
+	//Update position
+	XMVECTOR movement = XMLoadFloat3(&m_EmitterSettings.Velocity);
+	movement *= gameContext.pGameTime->GetElapsed();
+	XMVECTOR position = XMLoadFloat3(&m_VertexInfo.Position);
+	position += movement;
+	XMStoreFloat3(&m_VertexInfo.Position, position);
+
+	//Update color
+	float const particleLifePercent = m_CurrentEnergy / m_TotalEnergy;
+	m_VertexInfo.Color.w = particleLifePercent * 2.f;
+
+	//Update size
+	if (m_SizeGrow < 1.f)
+		m_VertexInfo.Size = m_InitSize - m_SizeGrow * particleLifePercent;
+	else
+		m_VertexInfo.Size = m_InitSize + m_SizeGrow * particleLifePercent;
+
+	
 }
 
 void Particle::Init(XMFLOAT3 initPosition)
 {
-	UNREFERENCED_PARAMETER(initPosition);
-	//TODO: See Lab9_2
+	m_IsActive = true;
+
+	//Init energy
+	m_TotalEnergy = randF(m_EmitterSettings.MinEnergy,m_EmitterSettings.MaxEnergy);
+	m_CurrentEnergy = m_TotalEnergy;
+
+	//Init position
+	XMFLOAT3 randomDir = {1.f,0.f,0.f};
+	XMVECTOR dirVector = XMLoadFloat3(&randomDir);
+	XMVECTOR const pos = XMLoadFloat3(&initPosition);
+	XMMATRIX const randomRotMatrix = XMMatrixRotationRollPitchYaw(randF(-XM_PI,XM_PI),randF(-XM_PI,XM_PI),randF(-XM_PI,XM_PI));
+	
+	dirVector =	XMVector3TransformNormal(dirVector,randomRotMatrix);
+	float const distance = randF(m_EmitterSettings.MinEmitterRange,m_EmitterSettings.MaxEmitterRange);
+	dirVector *= distance ;
+	XMStoreFloat3(&m_VertexInfo.Position,dirVector + pos);
+
+	//Init size
+	m_VertexInfo.Size = randF(m_EmitterSettings.MinSize,m_EmitterSettings.MaxSize);
+	m_InitSize = m_VertexInfo.Size;
+	m_SizeGrow = randF(m_EmitterSettings.MinSizeGrow,m_EmitterSettings.MaxSizeGrow);
+
+	//Init rotation
+	m_VertexInfo.Rotation = randF(-XM_PI,XM_PI);
 }
--- OverlordEngine/OverlordEngine/ParticleEmitterComponent.cpp
+++ OverlordEngine/OverlordEngine/ParticleEmitterComponent.cpp
@@ -19,41 +19,103 @@ ParticleEmitterComponent::ParticleEmitterComponent(std::wstring  assetFile, int 
 	m_LastParticleInit(0.0f),
 	m_AssetFile(std::move(assetFile))
 {
-	//TODO: See Lab9_2
+	for (int i = 0; i < m_ParticleCount; ++i)
+		m_Particles.push_back(new Particle(m_Settings));
 }
 
 ParticleEmitterComponent::~ParticleEmitterComponent()
 {
-	//TODO: See Lab9_2
+	for (auto& particle: m_Particles)
+		delete particle;
+
+	SafeRelease(m_pInputLayout);
+	SafeRelease(m_pVertexBuffer);
+	
 }
 
 void ParticleEmitterComponent::Initialize(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
+	LoadEffect(gameContext);
+	CreateVertexBuffer(gameContext);
+	m_pParticleTexture = ContentManager::Load<TextureData>(m_AssetFile);
 }
 
 void ParticleEmitterComponent::LoadEffect(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
+	m_pEffect = ContentManager::Load<ID3DX11Effect>(L"./Resources/Effects/ParticleRenderer.fx");
+
+	m_pDefaultTechnique = m_pEffect->GetTechniqueByIndex(0);
+
+	//load variables
+	m_pWvpVariable = m_pEffect->GetVariableByName("gWorldViewProj")->AsMatrix();
+	if (!m_pWvpVariable->IsValid())
+	{
+		std::wcout << L"m_pWvpVariable not valid\n";
+	}
+	m_pViewInverseVariable = m_pEffect->GetVariableByName("gViewInverse")->AsMatrix();
+	if (!m_pViewInverseVariable->IsValid())
+	{
+		std::wcout << L"m_pViewInverseVariable not valid\n";
+	}
+	m_pTextureVariable = m_pEffect->GetVariableByName("gParticleTexture")->AsShaderResource();
+	if (!m_pTextureVariable->IsValid())
+	{
+		std::wcout << L"m_pTextureVariable not valid\n";
+	}
+	
+	if(!EffectHelper::BuildInputLayout(gameContext.pDevice,m_pDefaultTechnique,&m_pInputLayout,m_pInputLayoutSize))
+		std::wcout << L"EffectHelper::BuildInputLayout --> did not succeed\n";
+		
 }
 
 void ParticleEmitterComponent::CreateVertexBuffer(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
+	SafeRelease(m_pVertexBuffer);
+
+	//Create dynamic buffer
+	D3D11_BUFFER_DESC buffDesc;
+	buffDesc.Usage = D3D11_USAGE_DYNAMIC;
+	buffDesc.ByteWidth = m_ParticleCount * sizeof(ParticleVertex);
+	buffDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	buffDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	buffDesc.MiscFlags = 0;
+
+	HRESULT const result = gameContext.pDevice->CreateBuffer(&buffDesc, nullptr, &m_pVertexBuffer);
+	if (result != S_OK)
+		Logger::LogHResult(result,L"ParticleEmitterComponent::CreateVertexBuffer",true);
+	
 }
 
 void ParticleEmitterComponent::Update(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
-
+	float const particleInterval = ((m_Settings.MaxEnergy + m_Settings.MinEnergy) * 0.5f) / static_cast<float>(m_ParticleCount)
+;
+	m_LastParticleInit += gameContext.pGameTime->GetElapsed();
+	
+	m_ActiveParticles = 0;
 	//BUFFER MAPPING CODE [PARTIAL :)]
-	//D3D11_MAPPED_SUBRESOURCE mappedResource;
-	//gameContext.pDeviceContext->Map(m_pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
-	//ParticleVertex* pBuffer = (ParticleVertex*) mappedResource.pData;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	gameContext.pDeviceContext->Map(m_pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	auto* pBuffer = (ParticleVertex*) mappedResource.pData;
+
+	for(auto& particle : m_Particles)
+	{
+		particle->Update(gameContext);
+		if (particle->IsActive())
+		{
+			pBuffer[m_ActiveParticles] = particle->GetVertexInfo();
+			++m_ActiveParticles;
+		}
+		else if(m_LastParticleInit >= particleInterval)
+		{
+			particle->Init(GetTransform()->GetPosition());
+			pBuffer[m_ActiveParticles] = particle->GetVertexInfo();
+			++m_ActiveParticles;
+			m_LastParticleInit = 0;
+		}
+	}
+
+	gameContext.pDeviceContext->Unmap(m_pVertexBuffer,0);
 }
 
 void ParticleEmitterComponent::Draw(const GameContext& )
@@ -61,6 +123,25 @@ void ParticleEmitterComponent::Draw(const GameContext& )
 
 void ParticleEmitterComponent::PostDraw(const GameContext& gameContext)
 {
-	UNREFERENCED_PARAMETER(gameContext);
-	//TODO: See Lab9_2
+	m_pWvpVariable->SetMatrix(&gameContext.pCamera->GetViewProjection()._11);
+	m_pViewInverseVariable->SetMatrix(&gameContext.pCamera->GetViewInverse()._11);
+	m_pTextureVariable->SetResource(m_pParticleTexture->GetShaderResourceView());
+
+	gameContext.pDeviceContext->IASetInputLayout(m_pInputLayout);
+
+	gameContext.pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_POINTLIST);
+
+	UINT stride = sizeof(ParticleVertex);
+	UINT offset = 0;
+	
+	gameContext.pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+	
+	D3DX11_TECHNIQUE_DESC techDesc;
+	m_pDefaultTechnique->GetDesc(&techDesc);
+	for (unsigned int i = 0; i < techDesc.Passes; ++i)
+	{
+		m_pDefaultTechnique->GetPassByIndex(i)->Apply(0, gameContext.pDeviceContext);
+		gameContext.pDeviceContext->Draw(m_ActiveParticles, 0);
+	}
+	
 }
